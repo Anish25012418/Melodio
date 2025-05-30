fd832520c6fb534c9500935226b61572af2c786d
anish <anishdangi.me@gmail.com>
2025-05-30 13:18:43 +0545

    feat: admin controller and route

---
+++ backend/src/routes/adminRoutes.js
@@ -0,0 +1,17 @@
+import {Router} from "express";
+import {protectedRoute, requireAdmin} from "../middleware/authMiddleware.js";
+import {checkAdmin, createAlbum, createSong, deleteAlbum, deleteSong} from "../controller/adminController.js";
+
+const router = Router();
+
+router.use(protectedRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("albums", createAlbum);
+router.delete("albums", deleteAlbum);
+
+export default router;
